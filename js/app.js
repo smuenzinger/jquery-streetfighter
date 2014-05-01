3bcaab64540ff8d44766f7f70ac91fab9090953f
smuenzinger <sarah.muenzinger@gmail.com>
2014-05-01 19:58:24 -0400

    Done
    I finished

--- js/app.js
+++ js/app.js
@@ -1,31 +1,45 @@
 $(document).ready(function() {
-   $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
-  })
-    .mouseleave(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-     playHadouken();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-	$('.hadouken').finish().show()
-    .animate(
-    	{'left': '300px'},
-  		500,
- 		function() {
-    		$(this).hide();
-    		$(this).css('left', '-212px');
-    	}
-    	);
-})
-    .mouseup(function(){
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+			);
+	})
+	.mouseup(function(){
     	$('.ryu-throwing').hide();
   		$('.ryu-ready').show();
-    });
-});
+    })
+    $(document).keydown(function(event){
+    	if (event.which == 88){
+    	$('.ryu-ready').hide();
+    	$('.ryu-still').hide();
+    	$('.ryu-cool').show();
+    	}
+    })
+    $(document).keyup(function(event){
+    	if (event.which == 88){
+    	$('.ryu-cool').hide();
+    	}
+    })
+}
+);
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
